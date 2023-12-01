72807e2d0209858dc7ac6f8742d4f26e16b4559a
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-11-30 21:16:45 -0500

    Update build-bb.sh

--- build-bb.sh
+++ build-bb.sh
@@ -9,7 +9,7 @@ rootdir=$(pwd)
 NewName=BoughBoot-$version-$boards_name
 cd armbian
 chmod a+x userpatches/customize-image.sh
-sudo --user $SUDO_USER ./compile.sh BOARD=$armbian_board BRANCH=legacy RELEASE=bookworm KERNEL_CONFIGURE=no CLEAN_LEVEL=alldebs,images,debs >/dev/null
+sudo --user $SUDO_USER ./compile.sh BOARD=$armbian_board BRANCH=legacy RELEASE=bookworm KERNEL_CONFIGURE=no CLEAN_LEVEL=alldebs,images,debs 
 cd ..
 cp armbian/output/images/$armbian_imgname bb/
 cd bb 
