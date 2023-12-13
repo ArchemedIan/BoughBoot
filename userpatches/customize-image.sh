61f7ca23155ca170ee80194c444e1967fcd64e6d
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-12-13 01:08:38 -0500

    Update customize-image.sh

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+RELEASE=$1
+FAMILY=$2
+BOARD=$3
+BUILD_DESKTOP=$4
+
 echo 15032385536B | tee /root/.rootfs_resize
 sed 's|echo -e "Old partition table:\\n"|unset newpartition|g' -i /usr/lib/armbian/armbian-resize-filesystem
 rm /root/.not_logged_in_yet
@@ -11,10 +17,10 @@ hostname -b BoughBoot
 apt update
 apt install -yy dialog xserver-xorg xinit xfce4 xfce4-session xfce4-goodies lightdm gnome-terminal dconf-cli
 dpkg-reconfigure lightdm
-#sed "s|auth.*required.*pam_succeed.* root quiet_success|#auth    required        pam_succeed_if.so user != root quiet_success|g" -i /etc/pam.d/lightdm-password
+
 sed "s|auth.*required.*pam_succeed.* root quiet_success|#auth    required        pam_succeed_if.so user != root quiet_success|g" -i /etc/pam.d/lightdm-autologin
 mv /etc/lightdm/lightdm.conf{,.orig}
-cat <<EOF > /etc/lightdm/lightdm.conf
+cat << EOF > /etc/lightdm/lightdm.conf
 [LightDM]
 
 [Seat:*]
@@ -26,10 +32,12 @@ autologin-user-timeout = 0
 [VNCServer]
 
 EOF
+
 cp /tmp/overlay/.dialogrc /root/.dialogrc
+cp /tmp/overlay/armbian_first_run.txt /boot
 mkdir -p /root/.config/autostart
 cp /tmp/overlay/BoughBoot.desktop /root/.config/autostart/BoughBoot.desktop || exit 1
-dconf load /org/gnome/terminal/legacy/profiles:/ < /tmp/overlay/gnome-terminal-profiles.dconf || exit 1
+
 
 # plymouth boot Theme
 mv /usr/share/desktop-base/debian-logos /usr/share/desktop-base/debian-logos.bak
